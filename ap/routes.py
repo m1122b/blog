ca5a560906f0c1bfff9fb3c8202399b5daa99a11
Miroslaw Biernacki <mb100773@gmail.com>
2022-05-15 21:41:15 +0200

    Add editing posts

--- ap/routes.py
+++ ap/routes.py
@@ -2,13 +2,45 @@
 # ap/routes.py
 
 
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from ap import app
 from ap.models import Post, db
 from ap.forms import PostForm
 
 
-@app.route("/")
+def create_edit_post(i, post_id):
+    if i == 1:
+        form = PostForm()
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                post = Post(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                    )
+                db.session.add(post)
+                db.session.commit()
+            else:
+                errors = form.errors
+            return redirect(url_for("index"))
+        return render_template("newpostform.html", form=form, errors=errors)
+    elif i == 2:
+        post = Post.query.filter_by(id=post_id).first_or_404()
+        form = PostForm(obj=post)
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                form.populate_obj(post)
+                db.session.commit()
+            else:
+                errors = form.errors
+            return redirect(url_for("index"))
+        return render_template("editpostform.html", form=form, errors=errors)
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     all_posts = Post.query.filter_by(is_published=True).order_by(Post.pub_date.desc())
 
@@ -16,7 +48,7 @@ def index():
 
 
 @app.route("/newpost/", methods=["GET", "POST"])
-def create_post():
+def create_post(): 
     form = PostForm()
     errors = None
     if request.method == 'POST':
@@ -30,7 +62,22 @@ def create_post():
             db.session.commit()
         else:
             errors = form.errors
+        return redirect(url_for("index"))
     return render_template("newpostform.html", form=form, errors=errors)
 
+
+@app.route("/editpost/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    form = PostForm(obj=post)
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(post)
+            db.session.commit()
+        else:
+            errors = form.errors
+        return redirect(url_for("index"))
+    return render_template("editpostform.html", form=form, errors=errors)
 
 
